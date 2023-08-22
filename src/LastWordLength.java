c85d6e0119d463f6ed15fc600a34f89385d2d02f
Brandon Stamba <brandonstamba@gmail.com>
2023-08-22 15:57:36 -0500

    Length of Last Word in a String - Version 3

--- src/LastWordLength.java
+++ src/LastWordLength.java
@@ -2,11 +2,21 @@ public class LastWordLength {
 
     public int lengthOfLastWord(String s) {
 
-        //Split our string into words using " " as our delimiter, then we will
-        //get the last word of the array and return it's length.
-        String[] sArr = s.split(" ");
-        String lastWord = sArr[sArr.length - 1];
-        int length = lastWord.length();
+        //We will trim any beginning or ending spaces and then traverse from the end of the string
+        //to the beginning, one we find a space we know we have reached the end of the last word.
+        s = s.trim();
+        int length = 0;
+
+        //Traverse from the end of the string to the beginning, once we find a space then
+        //we know we have reached the end of the last word.
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s.charAt(i) != ' '){
+                length++;
+            }
+            else{
+                break;
+            }
+        }
 
         return length;
     }
--- src/MainTester.java
+++ src/MainTester.java
@@ -1,7 +1,7 @@
 public class MainTester {
 
     public static void main(String[] args){
-        String s = "a";
+        String s = "hay you guise gobbledygook";
         LastWordLength lol = new LastWordLength();
         lol.lengthOfLastWord(s);
     }
